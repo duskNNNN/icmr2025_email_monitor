09658eaa32aedc09f4121d636c21fce04a5799ac
duskNNNN <wangc724@gmail.com>
2025-04-18 15:50:21 +0800

    first commit

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# icassp_monitor
-ICASSP2025 论文状态监控小脚本
-
+# icmr2025_monitor
+ICMR20252025 论文状态监控小脚本
+参考网址：https://github.com/liuup/icassp_monitor
 2 -> 论文接收  
 3 -> 论文拒绝  
 其他状态码 -> 还在审  
 
-所有状态码定义在 https://cmt3.research.microsoft.com/api/odata/ICASSP2025/SubmissionStatuses
+所有状态码定义在 https://cmt3.research.microsoft.com/api/odata/ICMR2025/SubmissionStatuses
 
 # 步骤
-1. 用浏览器登录官方的cmt系统 https://cmt3.research.microsoft.com/ICASSP2025/Submission/Index
+1. 用浏览器登录官方的cmt系统 https://cmt3.research.microsoft.com/ICMR2025/Submission/Index
 
-2. 用浏览器打开接口 https://cmt3.research.microsoft.com/api/odata/ICASSP2025/Submissions(xxxx) 这里的xxxx换成自己的论文id，比如1234
+2. 用浏览器打开接口 https://cmt3.research.microsoft.com/api/odata/ICMR2025/Submissions(xxxx) 这里的xxxx换成自己的论文id，比如1234
 
 3. 在接口的界面按键盘F12打开网页调试器，刷新一下界面，打开Submissions(xxxx)的接口
    <img width="533" alt="image" src="https://github.com/user-attachments/assets/6d92cbad-f59f-4fad-8b0a-6c0ced41d8ac" />
@@ -18,9 +18,10 @@ ICASSP2025 论文状态监控小脚本
 4. 把右边的Cookie这一坨复制到main.py的代码中
    <img width="1135" alt="image" src="https://github.com/user-attachments/assets/17b357a0-86aa-42ca-b9a7-dfde868b6439" />
 
-5. 下载本项目，修改main.py，把第8行改成自己的论文id，把第12行修改成自己的cookie，最后一行的轮询时间可以改成60，也就是60s，看你自己
+5. 下载本项目，修改main.py，把第8行的paper_id改成自己的论文id，把第10行修改成自己的cookie，
+   将第12行的email换成自己的邮箱，将第14行的sender_email换成往你邮箱发送的邮箱名，
+   将第15行的password改成邮箱对应的密码（此处需要开启对应邮箱的IMAP/SMTP服务），
+   第59行的xxx换成公共服务的域名（比如163邮箱：smtp.163.com）
 
 6. 运行`python main.py`
 
-# 运行效果
-<img width="561" alt="image" src="https://github.com/user-attachments/assets/8ba60b9d-1413-4951-acf9-9780bc06fe08" />
--- main.py
+++ main.py
@@ -2,34 +2,72 @@ import requests
 import json
 import time
 from datetime import datetime
-
-
-# 把这里的xxxxx改成自己的paper id，比如1234
-url = "https://cmt3.research.microsoft.com/api/odata/ICASSP2025/Submissions(xxxxxx)"
-
-# 把这里的xxxxx改成自己的cookie，具体填什么看readme
-cookies = {
-    "Cookie": "xxxxx"
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+paper_id = "xxx"
+cookies  = {
+    "Cookie": "xxx"
 }
+email = "xxx"
 
-# 可选：自定义 HTTP 请求头
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9"
-}
+sender_email = "xxx"
+password = "xxx"  
 
-while True:
+def get_status_id(paper_id,cookies,email_id,flag):
+    url = f"https://cmt3.research.microsoft.com/api/odata/ICMR2025/Submissions({paper_id})"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9"
+    }
     try:
         # 发送 GET 请求，携带 Cookie
         response = requests.get(url, cookies=cookies, headers=headers, timeout=10)
 
         if response.status_code == 200:
             res = json.loads(response.text)
-            print(f"{datetime.now()}, 当前论文状态: { res['StatusId'] }")
+            print(f"{datetime.now()}, 当前论文{paper_id}状态: { res['StatusId'] }")
+            status_id = res['StatusId']
+            if flag is False and status_id != 1:
+                mailer_send( status_id,email_id )
+                flag = True
+                return flag
+            if flag is True and status_id == 1:
+                mailer_send( status_id,email_id )
+                flag = False
+                return flag
         else:
             print(f"请求失败，HTTP 状态码：{response.status_code}")
+            return flag
 
     except requests.exceptions.RequestException as e:
         print(f"请求发生错误：{e}")
-    
-    time.sleep(60)
+
+def mailer_send(status_id,reciver_email):
+    message = MIMEMultipart()
+    message["Subject"] = "论文结果出炉！！！"
+    message["From"] = sender_email
+    message["To"] = reciver_email
+    if status_id == 1:
+        body = "坏了，状态重置了!!!"
+    elif status_id == 2:
+        body = "恭喜录用!!!"
+    elif status_id == 3:
+        body = "下次加油!!!"
+    message.attach(MIMEText(body, "plain"))
+    try:
+        with smtplib.SMTP_SSL("xxx", 465) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, reciver_email, message.as_string())
+            print("✅ 群发邮件成功！")
+    except Exception as e:
+        print(f"❌ 发送失败：{e}")    
+
+
+def main():
+    flag = False
+    while True:
+        flag = get_status_id(paper_id,cookies,email,flag)
+        time.sleep(60)
+        
+main()
